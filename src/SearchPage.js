6cc32d360bd0a08a16e1245cf1ebb657b107ddb9
adidayan17 <Adidayan17@gmail.com>
2022-01-26 21:34:01 +0200

    Full submit

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -7,11 +7,12 @@ import axios from "axios";
 
 class SearchPage extends React.Component {
     state = {
-        sales:[],
-        userSales :[],
+        sales: [],
+        userSales: [],
         searching: "",
-        border:""
+        border: ""
     }
+
     componentDidMount() {
         this.getSales()
     }
@@ -29,64 +30,66 @@ class SearchPage extends React.Component {
         return filtered;
     }
 
-    getSales=()=>{
+    getSales = () => {
         const cookies = new Cookies();
-        let token=cookies.get("token")
+        let token = cookies.get("token")
         axios.get("http://127.0.0.1:8989/get-all-sales")
-            .then(response=> {
+            .then(response => {
                 if (response.data) {
                     this.setState({
                         sales: response.data
                     })
                 }
             })
-        axios.get("http://127.0.0.1:8989/get-sales-by-user",{
-            params:{
-                token:token
+        axios.get("http://127.0.0.1:8989/get-sales-by-user", {
+            params: {
+                token: token
             }
-        }).then((response)=>{
+        }).then((response) => {
 
-            if(response.data){
-                this.setState({userSales:response.data})
-            }else {
-                this.setState({userSales:[]})
+            if (response.data) {
+                this.setState({userSales: response.data})
+            } else {
+                this.setState({userSales: []})
             }
 
         })
 
     }
 
-    doseSaleBelongToUser =(sale)=>{
+    doseSaleBelongToUser = (sale) => {
 
         let belong = false
-        this.state.userSales.map((userSale)=>{
-            return(
+        this.state.userSales.map((userSale) => {
+            return (
                 <div>{
-                    userSale.id == sale.id   &&
+                    userSale.id == sale.id &&
                     <div>{
                         belong = true
                     }
                     </div>}
                 </div>
-            )})
+            )
+        })
 
         return belong
     }
 
 
-
     render() {
-        return(
-            <div style={{textAlign:"center"}}>
-                <h1>Search For Sales Here  <BiSearch/></h1>
+        return (
+            <div style={{textAlign: "center"}}>
+                <h1>Search For Sales Here <BiSearch/></h1>
                 <p>
                     <input type={"text"} onChange={this.searching} placeholder={"Your text here ...."}/></p>
 
                 {
                     this.filter().map(sale => {
                         return (
-                            <Sale data={sale} key={sale.id} border={sale.availableForAll!=0?"green": this.doseSaleBelongToUser(sale)?"green":"red"}/>
-                        ) })
+                            <Sale data={sale} key={sale.id}
+                                  border={sale.availableForAll != 0 ? "green" : this.doseSaleBelongToUser(sale) ? "green" : "red"}/>
+                        )
+                    })
                 }
                 <p><span>Green means that you can use this promotions ,and red means that you can't</span></p>
             </div>
@@ -95,4 +98,5 @@ class SearchPage extends React.Component {
     }
 
 }
+
 export default SearchPage;
